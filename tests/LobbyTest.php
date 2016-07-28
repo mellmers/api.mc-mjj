3a568ccd9254ce473dfe5bbfdc9c506b41e025dd
Thorben Werner <w.thorben94@gmail.com>
2016-07-28 13:40:46 +0200

    enabled code coverage

--- tests/LobbyTest.php
+++ tests/LobbyTest.php
@@ -27,33 +27,45 @@ WHERE o.id = :id';
 
 
     public function testGetLobbyByID() {
-        $appMock = $this->getMockBuilder(\Silex\Application::class)->disableOriginalConstructor()->getMock();
-        $appMock->expects($this->never())->method('abort');
-
         $connectionMock = $this->getMockBuilder(Connection::class)->disableOriginalConstructor()->getMock();
         $connectionMock->expects($this->any())->method('fetchAll')->will($this->returnCallback(array($this, 'connectionMockCallback')));
+
+        $appMock = $this->getMockBuilder(\Silex\Application::class)->disableOriginalConstructor()->getMock();
+        $appMock->expects($this->never())->method('abort');
 
         $lobbyRepo = new \projectx\api\lobby\LobbyRepository($appMock, $connectionMock);
         $this->assertNotEmpty($lobbyRepo->getById(1));
     }
 
+    public function testGetBetByUser() {
+        $connectionMock = $this->getMockBuilder(Connection::class)->disableOriginalConstructor()->getMock();
+        $connectionMock->expects($this->any())->method('fetchAll')->will($this->returnCallback(array($this, 'connectionMockCallback')));
+
+        $appMock = $this->getMockBuilder(\Silex\Application::class)->disableOriginalConstructor()->getMock();
+        $appMock->expects($this->never())->method('abort');
+
+        $betRepo = new \projectx\api\bet\BetRepository($appMock, $connectionMock);
+        $this->assertNotEmpty($betRepo->getByUserId(1));
+    }
+
+
     public function connectionMockCallback($foo) {
-        if(strcmp($foo, $this->betSql) === 0) {
-            return [['userid' => 1, 'lobbyId' => 1, 'amount' => 0, 'team' => 0]];
+        if(strpos($foo, 'bet') !== false) {//strcmp($foo, $this->betSql) === 0) {
+            return [['userId' => 1, 'lobbyId' => 1, 'amount' => 0, 'team' => 0]];
         }
-        else if(strcmp($foo, $this->gameSql) === 0) {
+        else if(strpos($foo, 'game') !== false) {//strcmp($foo, $this->gameSql) === 0) {
             return [['id' => 1, 'name' => "someName", 'typ' => "sometype", 'icon' => "someIcon", 'rules' => 'someRules', 'genre' => "somegenre", 'timelimit' => 1000]];
         }
-        else if(strcmp($foo, $this->gameAccountSql) === 0) {
+        else if(strpos($foo, 'gameaccount') !== false) {//strcmp($foo, $this->gameAccountSql) === 0) {
             return [['userid' => 1, 'userIdentifier' => "username", 'gameaccountTypeId' => 1]];
         }
-        else if(strcmp($foo, $this->gameAccounTypeSql) === 0) {
+        else if(strpos($foo, 'gameaccountType') !== false) {//strcmp($foo, $this->gameAccounTypeSql) === 0) {
             return [['id' => 1, 'name' => "gameAccountTypeName", 'icon' => 'someIcon']];
         }
-        else if(strcmp($foo, $this->lobbySql) === 0) {
+        else if(strpos($foo, 'lobby') !== false) {//strcmp($foo, $this->lobbySql) === 0) {
             return [['id' => 1, 'ownerId' => 1, 'gameId' => 1, 'winnerteam' => 1, 'createdAt' => 0, 'starttime' => 0, 'endtime' => 0]];
         }
-        else if(strcmp($foo, $this->userSql) === 0) {
+        else if(strpos($foo, 'user') !== false) {//strcmp($foo, $this->userSql) === 0) {
             return [['id' => 1, 'email' => "someMail", 'username' => "username", 'trusted' => 1, 'password' => 'somePassWord', 'icon' => "someIcon", 'coins' => 0, 'createdAt' => 0]];
         }
         else {
