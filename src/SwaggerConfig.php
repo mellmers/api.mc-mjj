2edd07dab58366fc4da181e6fab482dda78e3461
Moritz <mellmers@users.noreply.github.com>
2016-07-29 16:29:43 +0200

    - updated swaggerconfig

--- src/SwaggerConfig.php
+++ src/SwaggerConfig.php
@@ -33,20 +33,20 @@
  *         @SWG\Property(property="trusted", type="boolean"),
  *         @SWG\Property(property="icon",  type="string"),
  *         @SWG\Property(property="coins", type="integer"),
- *         @SWG\Property(property="createdAt", type="string")
+ *         @SWG\Property(property="createdAt", type="integer")
  *     ),
  *     @SWG\Definition(
  *         definition="Bet",
  *         @SWG\Property(property="userId", type="string"),
  *         @SWG\Property(property="lobbyId", type="string"),
  *         @SWG\Property(property="amount",  type="integer"),
- *         @SWG\Property(property="team", type="boolean")
+ *         @SWG\Property(property="team", type="integer")
  *     ),
  *     @SWG\Definition(
  *         definition="Game",
  *         @SWG\Property(property="id", type="string"),
  *         @SWG\Property(property="name", type="string"),
- *         @SWG\Property(property="typ", type="string"),
+ *         @SWG\Property(property="type", type="string"),
  *         @SWG\Property(property="icon",  type="string"),
  *         @SWG\Property(property="rules", type="string"),
  *         @SWG\Property(property="genre",  type="string"),
@@ -70,10 +70,10 @@
  *         @SWG\Property(property="ownerId", type="string"),
  *         @SWG\Property(property="gameId", type="string"),
  *         @SWG\Property(property="winnerTeam",  type="integer"),
- *         @SWG\Property(property="createdAt", type="boolean"),
+ *         @SWG\Property(property="createdAt", type="integer"),
  *         @SWG\Property(property="starttime",  type="string"),
  *         @SWG\Property(property="endtime", type="integer"),
- *         @SWG\Property(property="users", type="integer")
+ *         @SWG\Property(property="users", type="array")
  *     )
  * )
  *
@@ -82,17 +82,17 @@
  * @SWG\Tag(name="game", description="All about games")
  * @SWG\Tag(name="gameAccount", description="All about gameAccounts")
  * @SWG\Tag(name="gameAccountType", description="All about gameAccountTypes")
- * @SWG\Tag(name="lobby", description="All about lobbys")
- * @SWG\Tag(name="user", description="All about Users")
+ * @SWG\Tag(name="lobby", description="All about lobbies")
+ * @SWG\Tag(name="user", description="All about users")
  *
  *
  * @SWG\Parameter(name="gameId", in="path", type="string", description="")
  * @SWG\Parameter(name="lobbyId", in="path", type="string", description="")
  * @SWG\Parameter(name="userId", in="path", type="string", description="")
- * @SWG\Parameter(name="gameAccId", in="path", type="string", description="")
+ * @SWG\Parameter(name="gameAccountId", in="path", type="string", description="")
  * @SWG\Parameter(name="gameAccountTypeId", in="path", type="string", description="")
- * @SWG\Parameter(name="gameAccount", in="path", type="integer", format="int32")
- * @SWG\Parameter(name="betId", type="integer", format="int32", in="path")
+ * @SWG\Parameter(name="gameAccount", in="path", type="Gameaccount", description="")
+ * @SWG\Parameter(name="betId", in="path", type="string")
  *
  * @SWG\Parameter(name="type", in="path", type="string", description="")
  * @SWG\Parameter(name="genre", in="path", type="string", description="")
